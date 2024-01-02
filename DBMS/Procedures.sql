4efd9a9cb85c43eedddbf7c48e755968d166fc5c
Nguyễn Trần Châu Minh <92373278+ChouMean@users.noreply.github.com>
2024-01-02 12:08:34 +0700

    Update bit thanhtoan, add proc, check privilege

--- DBMS/Procedures.sql
+++ DBMS/Procedures.sql
@@ -1,8 +1,53 @@
+﻿CREATE DATABASE dcBooking
+GO
 USE dcBooking
 GO
 
-SELECT *
-FROM VAITRO
+-- Kiểm tra thông tin ngày sinh khách hàng
+-- Kiểm tra nếu khách hàng đã đặt lịch sẽ không thể đổi lịch làm việc
 
-USE master
+CREATE PROC sp_DangNhap
+	@SDT PHONE,
+	@MK MTEXT,
+	@MATK INT OUTPUT,
+	@MAVT INT OUTPUT
+AS
+BEGIN
+	SET @MATK = NULL
+	SET @MAVT = NULL
+
+	IF NOT EXISTS (
+		SELECT SDT = @SDT
+		FROM TAIKHOAN
+	)
+		BEGIN
+			PRINT N'Không tìm thấy tài khoản'
+			RETURN 0
+		END
+	ELSE
+	IF NOT EXISTS (
+		SELECT SDT = @SDT, MK = @MK
+		FROM TAIKHOAN
+	)
+		BEGIN
+			PRINT N'Sai mật khẩu'
+			RETURN 0
+		END
+    
+	SELECT @MATK = MATK, @MAVT = MAVT
+	FROM TAIKHOAN
+    WHERE SDT = @SDT AND MK = @MK
+
+    PRINT N'Đăng nhập thành công'
+    RETURN 1
+END
 GO
+
+DECLARE @SDT PHONE = '0338540248'
+DECLARE @MK MTEXT = '21126030'
+DECLARE @MATK INT
+DECLARE @MAVT INT
+
+EXEC sp_DangNhap @SDT, @MK, @MATK OUTPUT, @MAVT OUTPUT
+PRINT N'Mã tài khoản: ' + CAST(@MATK AS INT)
+PRINT N'Vai trò: ' + CAST(@MAVT AS INT)
